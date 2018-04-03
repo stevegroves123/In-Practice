1ecd56f15b55360d25927cc806c2de7f3c381b02
Steve Groves <stevegroves123@gmail.com>
2018-04-03 22:27:22 +0100

    Initial Commit

---
+++ In-Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  In-Practice
+//
+//  Created by steve groves on 03/04/2018.
+//  Copyright © 2018 steve groves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
